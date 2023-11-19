2f37b5b81c95508cf230fa7ce3761aa01df34f06
Peter Bittner <django@bittner.it>
2023-11-19 19:32:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example
+
+Python Biella example project showing off testing in Python supported by CI automation.
+
+https://pythonbiellagroup.gitlab.io/testinginpython
---
+++ example.py
@@ -0,0 +1,13 @@
+"""A simple example module."""
+
+def square(a):
+    """Calculate the surface of a square.
+
+    >>> square(2) == 4
+    True
+    >>> square(0) == 0
+    True
+    >>> square(-1) == 1
+    True
+    """
+    return a * a
